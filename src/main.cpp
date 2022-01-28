00e356c38b2a107ae0494ecb2a2edaeb29e0eb13
Ivan_Busygin <busygin_02@list.ru>
2022-01-28 17:13:13 +0300

    Пара поправок.

--- src/main.cpp
+++ src/main.cpp
@@ -22,18 +22,18 @@ struct windowSize {
 };
 
 static void initWindow(
-  struct window* const window, struct windowSize* const size,
+  struct window* const window, struct windowSize size,
   const string title, const int style
 ) {
-  window->cellsWidth  = size->width / size->cellSize;
-  window->cellsHeight = size->height / size->cellSize;
-  size->width  = window->cellsWidth * size->cellSize;
-  size->height = window->cellsHeight * size->cellSize;
-  window->renderWindow.create(VideoMode(size->width, size->height), title, style);
+  window->cellsWidth  = size.width / size.cellSize;
+  window->cellsHeight = size.height / size.cellSize;
+  size.width  = window->cellsWidth * size.cellSize;
+  size.height = window->cellsHeight * size.cellSize;
+  window->renderWindow.create(VideoMode(size.width, size.height), title, style);
   window->renderWindow.setFramerateLimit(60);
   window->texture.create(window->cellsWidth, window->cellsHeight);
   window->sprite = Sprite(window->texture.getTexture());
-  window->sprite.setScale(size->cellSize, size->cellSize);
+  window->sprite.setScale(size.cellSize, size.cellSize);
 }
 
 static void drawWindow(struct window* const window, Shader &shader, const Vec4 top, const Vec4 right) {
@@ -51,8 +51,8 @@ int main() {
   const float mtrHeight = 1.0f;  // Половина высоты матрицы (виртуального экрана в пространстве).
   struct windowSize mainWindow = { .width = 850, .cellSize = 4 };
   mainWindow.height = mainWindow.width / GOLDEN;
-  struct windowSize addWindow  = { .width = 700, .cellSize = 4 };
-  addWindow.height  = addWindow.width / GOLDEN;
+  struct windowSize additionalWindow  = { .width = 700, .cellSize = 4 };
+  additionalWindow.height  = additionalWindow.width / GOLDEN;
 
   // Корректировка размеров окон на случай, если экран слишком маленький.
   const unsigned int windowTitleHeight = 37, taskBarHeight = 60,
@@ -61,29 +61,29 @@ int main() {
 
   const float decreaseFactor = min(min(
     1.0f,
-    (float) screenHeight / (mainWindow.height + addWindow.height)
+    (float) screenHeight / (mainWindow.height + additionalWindow.height)
   ), min(
-    (float) screenWidth / 2 / addWindow.width,
+    (float) screenWidth / 2 / additionalWindow.width,
     (float) screenWidth / mainWindow.width
   ));
 
   mainWindow.width *= decreaseFactor; mainWindow.height *= decreaseFactor;
-  addWindow.width  *= decreaseFactor; addWindow.height  *= decreaseFactor;
+  additionalWindow.width  *= decreaseFactor; additionalWindow.height  *= decreaseFactor;
 
   // Инициализация окон.
   struct window windowYXZ;
-  initWindow(&windowYXZ, &mainWindow, "Main section", Style::Close);
+  initWindow(&windowYXZ, mainWindow, "Main section", Style::Close);
   controlsInit(windowYXZ.renderWindow); // Управление будет привязано к этому окну.
 
   struct window windowYWZ;
-  initWindow(&windowYWZ, &addWindow, "", Style::None);
+  initWindow(&windowYWZ, additionalWindow, "", Style::None);
 
   struct window windowYXW;
-  initWindow(&windowYXW, &addWindow, "", Style::None);
+  initWindow(&windowYXW, additionalWindow, "", Style::None);
 
   // Положение окон на экране.
-  const unsigned int indentY = (screenHeight - mainWindow.height - addWindow.height) / 3;
-  const unsigned int indentX = (screenWidth - addWindow.width * 2) / 3;
+  const unsigned int indentY = (screenHeight - mainWindow.height - additionalWindow.height) / 3;
+  const unsigned int indentX = (screenWidth - additionalWindow.width * 2) / 3;
 
   windowYXZ.renderWindow.setPosition(Ivec2(
     (screenWidth - mainWindow.width) / 2, indentY
@@ -92,7 +92,7 @@ int main() {
     indentX, mainWindow.height + windowTitleHeight + indentY * 2
   ));
   windowYXW.renderWindow.setPosition(Ivec2(
-    addWindow.width + indentX * 2, mainWindow.height + windowTitleHeight + indentY * 2
+    additionalWindow.width + indentX * 2, mainWindow.height + windowTitleHeight + indentY * 2
   ));
 
   // Инициализация шейдера.
